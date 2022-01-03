905f7d2506b794be633e251aa19aede7ea13f1e9
name <email@example.com>
2022-01-03 12:18:21 +0800

    增加参数"控制台验证码"：consoleCode，默认关闭false

--- App.go
+++ App.go
@@ -1,34 +1,17 @@
 package main
 
 import (
+	"docker-manager/conf"
 	"docker-manager/data"
 	"docker-manager/service"
 	"docker-manager/web"
-	"flag"
 	"github.com/xiaojun207/go-base-utils/utils"
-	"log"
 )
-
-var (
-	driverName    = "sqlite3"
-	dataSourceUrl = "data/database.db"
-	useCache      = "true"
-)
-
-func ParseParam() {
-	flag.StringVar(&driverName, "driverName", driverName, "driverName, eg.: mysql or sqlite3")
-	flag.StringVar(&useCache, "useCache", useCache, "useCache, eg.: true or false")
-	flag.StringVar(&dataSourceUrl, "dataSourceUrl", dataSourceUrl, "dataSourceUrl, eg.: root:123456@(127.0.0.1:3306)/docker-manager?charset=utf8")
-	flag.Parse()
-	log.Println("flag.driverName:", driverName)
-	log.Println("flag.useCache:", useCache)
-	log.Println("flag.dataSourceUrl:", utils.SubstrAfter(dataSourceUrl, "@"))
-}
 
 func main() {
-	ParseParam()
+	conf.ParseParam()
 
-	data.InitDB(driverName, dataSourceUrl, utils.StrToBool(useCache))
+	data.InitDB(conf.DriverName, conf.DataSourceUrl, utils.StrToBool(conf.UseCache))
 	service.InitTokenHelper()
 	service.LoadWhiteList()
 	service.LoadContainerMap()
