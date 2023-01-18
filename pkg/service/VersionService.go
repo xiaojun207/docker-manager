995105b89b026a15184b8ce9d42a50b31db06956
xiaojun207 <xiaojun207@126.com>
2023-01-18 15:37:00 +0800

    修改版本信息

--- pkg/service/VersionService.go
+++ pkg/service/VersionService.go
@@ -14,43 +14,47 @@ import (
 
 // go build -a -ldflags "-X docker-manager/service.Version=$VERSION" -o docker-manager
 var (
-	Version    = "1.4.7"
+	Version    = "1.5.0"
 	Date       = "20220815"
 	CommitHash = ""
 )
 
 type HubTag struct {
-	Count    int         `json:"count"`
-	Next     interface{} `json:"next"`
-	Previous interface{} `json:"previous"`
-	Results  []struct {
-		Creator int `json:"creator"`
-		Id      int `json:"id"`
-		Images  []struct {
-			Architecture string      `json:"architecture"`
-			Features     string      `json:"features"`
-			Variant      interface{} `json:"variant"`
-			Digest       string      `json:"digest"`
-			Os           string      `json:"os"`
-			OsFeatures   string      `json:"os_features"`
-			OsVersion    interface{} `json:"os_version"`
-			Size         int         `json:"size"`
-			Status       string      `json:"status"`
-			LastPulled   time.Time   `json:"last_pulled"`
-			LastPushed   time.Time   `json:"last_pushed"`
-		} `json:"images"`
-		LastUpdated         time.Time `json:"last_updated"`
-		LastUpdater         int       `json:"last_updater"`
-		LastUpdaterUsername string    `json:"last_updater_username"`
-		Name                string    `json:"name"`
-		Repository          int       `json:"repository"`
-		FullSize            int       `json:"full_size"`
-		V2                  bool      `json:"v2"`
-		TagStatus           string    `json:"tag_status"`
-		TagLastPulled       time.Time `json:"tag_last_pulled"`
-		TagLastPushed       time.Time `json:"tag_last_pushed"`
-		MediaType           string    `json:"media_type"`
-	} `json:"results"`
+	Count    int            `json:"count"`
+	Next     interface{}    `json:"next"`
+	Previous interface{}    `json:"previous"`
+	Results  []HubTagResult `json:"results"`
+}
+
+type HubTagResult struct {
+	Creator             int           `json:"creator"`
+	Id                  int           `json:"id"`
+	Images              []HubTagImage `json:"images"`
+	LastUpdated         time.Time     `json:"last_updated"`
+	LastUpdater         int           `json:"last_updater"`
+	LastUpdaterUsername string        `json:"last_updater_username"`
+	Name                string        `json:"name"`
+	Repository          int           `json:"repository"`
+	FullSize            int           `json:"full_size"`
+	V2                  bool          `json:"v2"`
+	TagStatus           string        `json:"tag_status"`
+	TagLastPulled       time.Time     `json:"tag_last_pulled"`
+	TagLastPushed       time.Time     `json:"tag_last_pushed"`
+	MediaType           string        `json:"media_type"`
+}
+
+type HubTagImage struct {
+	Architecture string      `json:"architecture"`
+	Features     string      `json:"features"`
+	Variant      interface{} `json:"variant"`
+	Digest       string      `json:"digest"`
+	Os           string      `json:"os"`
+	OsFeatures   string      `json:"os_features"`
+	OsVersion    interface{} `json:"os_version"`
+	Size         int         `json:"size"`
+	Status       string      `json:"status"`
+	LastPulled   time.Time   `json:"last_pulled"`
+	LastPushed   time.Time   `json:"last_pushed"`
 }
 
 func init() {
