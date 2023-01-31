058dbfc1d4f91281f82fc8f4fac9965176b2ac89
xiaojun207 <xiaojun207@126.com>
2023-01-31 23:55:41 +0800

    优化界面多语言

---
+++ views/assets/redirect-274cdd07.js
@@ -0,0 +1,1 @@
+import{d as a,u as c,g as p,_ as u,o as i,c as _}from"./index-69997a90.js";const d=a({name:"Redirect",setup(){const{currentRoute:t,replace:r}=c(),{params:n,query:o}=p(t),{path:e}=n,s=Array.isArray(e)?e.join("/"):e;return r({path:"/"+s,query:o}),{}}});function f(t,r,n,o,e,s){return i(),_("div")}const l=u(d,[["render",f]]);export{l as default};
---
+++ views/assets/task-580f04ce.js
@@ -0,0 +1,1 @@
+import{q as r}from"./index-69997a90.js";function s(t){return r({url:"/mgr/tasks",method:"get",params:t})}function o(t){return r({url:"/mgr/task/del",method:"post",data:t})}export{o as d,s as g};
